2ba0786b80705aa808aa7fb63bab68d4be360e96
danya-purple02 <iza.darkwoodenn@gmail.com>
2024-11-08 15:29:04 +0300

    task 1 point 2 done

--- lw7/lw7/main.cpp
+++ lw7/lw7/main.cpp
@@ -6,16 +6,31 @@ using namespace std;
 int** create_adjacency_matrix(int vertexes);
 int cout_matrix(int vertexes, int** G);
 
+void DFS_matrix(int** g, int s, int size, int* vis);
+
 void main()
 {
 	int** M;
 	int vertexes = 0;
+	int to_start_with = 0;
 
 	cout << "input graph size: ";
 	cin >> vertexes;
 
 	M = create_adjacency_matrix(vertexes);
 	cout_matrix(vertexes, M);
+
+	int* visited = new int[vertexes];
+	for (int i = 0; i < vertexes; i++)
+	{
+		visited[i] = 0;
+	}
+
+	cout << endl << "input input number of vertex to star with: ";
+	cin >> to_start_with;
+
+	cout << endl << "First-deep search: " << endl;
+	DFS_matrix(M, to_start_with, vertexes, visited);
 
 	cout << endl;
 	return;
@@ -67,4 +82,17 @@ int cout_matrix(int vertexes, int** G)
 		cout << endl;
 	}
 	return 1;
+}
+
+void DFS_matrix(int** g, int s, int size, int* vis)
+{
+	vis[s] = 1;
+	cout << s << " -> ";
+	for (int i = 0; i < size; i++)
+	{
+		if (g[s][i] == 1 && vis[i] == 0)
+		{
+			DFS_matrix(g, i, size, vis);
+		}
+	}
 }
