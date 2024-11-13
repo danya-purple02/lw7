c818ec93fbfb1bc1d547d9a5105f8b0b688815ab
danya-purple02 <iza.darkwoodenn@gmail.com>
2024-11-13 19:53:57 +0300

    task 2 point 1 done

--- lw7/lw7/main.cpp
+++ lw7/lw7/main.cpp
@@ -1,7 +1,10 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <stack>
 
 using namespace std;
+
+#define MAX_VERTICES 20
 
 struct Node
 {
@@ -15,15 +18,15 @@ struct Graph
 	struct Node** list;
 };
 
+struct Stack 
+{
+	int items[MAX_VERTICES];
+	int top;
+};
+
 int** create_adjacency_matrix(int vertexes);
 int cout_matrix(int vertexes, int** G);
-void DFS_matrix(int** g, int s, int size, bool* vis);
-
-struct Graph* create_adjacency_list(int vertexes);
-struct Node* create_vertex(int vertex);
-void connect_vertexes(struct Graph* graph, int coll, int dest);
-void cout_adjacency_list(struct Graph* graph);
-void DFS_list(struct Graph* G, int s, int size, bool* vis);
+void DFS_matrix_stack(int** g, int s, int size, bool* vis);
 
 void main()
 {
@@ -47,31 +50,7 @@ void main()
 	cin >> to_start_with;
 
 	cout << endl << "First-deep matrix search: " << endl;
-	DFS_matrix(M, to_start_with, vertexes, visited);
-	cout << endl;
-
-	struct Graph* G1 = create_adjacency_list(vertexes);
-	for (int i = 0; i < vertexes; i++)
-	{
-		for (int j = 0; j < vertexes; j++)
-		{
-			if (M[i][j] == 1)
-			{
-				connect_vertexes(G1, i, j);
-			}
-		}
-	}
-	cout_adjacency_list(G1);
-	cout << endl << "---------------------------------" << endl;
-
-	for (int i = 0; i < vertexes; i++)
-	{
-		visited[i] = false;
-	}
-	cout << endl << "input input number of vertex to star with: ";
-	cin >> to_start_with;
-	cout << endl << "First-deep list search: " << endl;
-	DFS_list(G1, to_start_with, vertexes, visited);
+	DFS_matrix_stack(M, to_start_with, vertexes, visited);
 	cout << endl;
 	return;
 }
@@ -124,95 +103,22 @@ int cout_matrix(int vertexes, int** G)
 	return 1;
 }
 
-void DFS_matrix(int** g, int s, int size, bool* vis)
+void DFS_matrix_stack(int** g, int s, int size, bool* vis)
 {
+	stack<int> stack;
+	stack.push(s);
 	vis[s] = true;
-	cout << s << " -> ";
-	for (int i = 0; i < size; i++)
-	{
-		if (g[s][i] == 1 && vis[i] == 0)
-		{
-			DFS_matrix(g, i, size, vis);
-		}
-	}
-}
 
-struct Graph* create_adjacency_list(int vertexes)
-{
-	struct Graph* graph = new struct Graph;
-	graph->vertexes_amount = vertexes;
-	graph->list = new struct Node* [vertexes];
-	for (int i = 0; i < vertexes; i++)
-	{
-		graph->list[i] = new struct Node[vertexes];
-	}
+	while (!stack.empty()) {
+		int v = stack.top();
+		stack.pop();
+		cout << v << " -> ";
 
-	for (int i = 0; i < vertexes; i++)
-	{
-		graph->list[i] = create_vertex(i);
-	}
-
-	return graph;
-}
-
-struct Node* create_vertex(int vertex)
-{
-	struct Node* new_node = new struct Node;
-	new_node->vertex = vertex;
-	new_node->next = NULL;
-	return new_node;
-}
-
-void connect_vertexes(struct Graph* graph, int coll, int dest)
-{
-	struct Node* new_node = create_vertex(dest);
-	int i = 0;
-	while (graph->list[i]->vertex != coll)
-	{
-		i++;
-	}
-
-	struct Node* tmp = graph->list[i];
-	while (tmp->next != NULL)
-	{
-
-		tmp = tmp->next;
-	}
-
-	tmp->next = new_node;
-}
-
-void cout_adjacency_list(struct Graph* graph)
-{
-	cout << endl << "adjacency list:" << endl;
-	struct Node* tmp;
-	for (int i = 0; i < graph->vertexes_amount; i++)
-	{
-		tmp = graph->list[i];
-		while (tmp)
-		{
-			cout << tmp->vertex;
-			tmp = tmp->next;
-			if (tmp != NULL)
-			{
-				cout << " -> ";
+		for (int i = size - 1; i >= 0; i--) {
+			if (g[v][i] == 1 && !vis[i]) {
+				stack.push(i);
+				vis[i] = true;
 			}
 		}
-		cout << endl;
-	}
-}
-
-void DFS_list(struct Graph* G, int s, int size, bool* vis) 
-{
-	vis[s] = true;
-	cout << s << " -> ";
-	Node* adjNode = G->list[s];
-	while (adjNode != NULL) 
-	{
-		int v = adjNode->vertex;
-		if (!vis[v]) {
-			DFS_list(G, v, size, vis);
-		}
-		adjNode = adjNode->next;
 	}
 }
